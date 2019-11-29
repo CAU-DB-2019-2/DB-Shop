9bad7ab07342f6a0ba3117249f20200f200cf0ff
Kim Jin Kwan <kkole3897@naver.com>
2019-11-29 17:07:00 +0900

    update 3.sql

--- 3.sql
+++ 3.sql
@@ -2,6 +2,6 @@ select p.product_name
 from Brand b, Register_product r, Product p
 where b.brand_id = r.brand_id and 
 	r.product_id = p.product_id and
-	b.brand_name = '아디다스' and
-	p.maincategory = '' and
+	b.brand_name = '에르메스' and
+	p.maincategory = '상의' and
 	p.price <= 50000;
