1498e47d083d89757c7bd514d8113df730d6d145
Kim Jin Kwan <kkole3897@naver.com>
2019-11-29 14:07:51 +0900

    Fixed untracked files

--- 2.sql
+++
@@ -1,4 +0,0 @@
-select product_id, sum(quntity)
-from purchase_info
-group by product_id
-order by sum(quntity)
--- 5.sql
+++
@@ -1,4 +0,0 @@
-select count(P.order_id)
-from User_Info as U, Purchase_Info as P
-where U.user_id = P.user_id and P.total_price > 100000
-group by U.address1
